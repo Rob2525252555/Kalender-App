838fc106080e016f065d0c69b485ac1f92a053aa
Robin Hack <robin.hack@stud.uni-heidelberg.de>
2026-03-05 14:43:40 +0100

    docs: add JSdoc comments to dom.create function

--- public/js/core/dom.js
+++ public/js/core/dom.js
@@ -1,4 +1,37 @@
+/**
+ * @module dom
+ * Hilfsmodul zur Erstellung von dynamischen HTML-Elementen
+ */
+
 const dom = {
+    /**
+     * Erstelle ein HTML-Element und füge es optional in die DOM-Struktur ein
+     * 
+     * HTML-Eigenschaften für das neue Element
+     * @param {string} [tagName = 'div'] - Tag-Name des HTML-Elements
+     * @param {string} [innerText = ''] - Textinhalt des HTML-Elements
+     * @param {string} [innerHTML = ''] - HTML-Inhalt des HTML-Elements. Hat Vorrang vor innerText.
+     * @param {string} [id = ''] - Id des HTML Elements
+     * @param {string} [name = ''] - Name-Attribut (z.B. für Formulare)
+     * @param {string} [type = ''] - Type für Input-Elemente oder Buttons
+     * @param {string} [value = ''] - Wert eines HTML-Elements (z.B. input)
+     * @param {string} [className = ''] - CSS-Klasse/n einem HTML-Element zuweisen (überschreibt alle vorhandenen Klassen)
+     * @param {string[]} [classList = []] - Hinzufügen, entfernen oder toggeln von Css Klassen
+     * @param {HTMLElement} [parent = null] - Parent Element des neuen HTML-Elements
+     * @param {'append'|'prepend'|'before'|'after'} [insert = 'append'] - Art wie das HTML-Element an das Parent-Element angehängt wird.
+     * @param {object<string, Function>} [eventListeners = {}] - Key-Value-Paare für Eventlistener. Key = Eventname, Value = Funktion.
+     * 
+     * @returns {HTMLElement} - Das neu erstellte HTML-Element
+     * 
+     * @example
+     * dom.create({
+     * tagName: 'button',
+     * innerText: 'click button',
+     * classList: ['btn', 'big-button'],
+     * parent: document.body,
+     * eventListeners: { click: () => foo() }
+     * });
+     */
     create({
         tagName = 'div',
         
