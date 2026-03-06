526955928aee5a80ef7342699971aa7cbc25080e
Robin Hack <robin.hack@stud.uni-heidelberg.de>
2026-03-06 13:10:09 +0100

    feat: create elements module to store DOM references

---
+++ public/js/core/elements.js
@@ -0,0 +1,12 @@
+const elements = {
+    mainContainer: null,
+    header: null,
+    btnArrowLeft: null,
+    btnArrowRight: null,
+    currentMonthAndYear: null,
+    calendarMainContainer: null,
+    calendarCells: [],
+    dayNumberContainer: []
+};
+
+export default elements;
