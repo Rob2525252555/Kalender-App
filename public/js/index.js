0057e01da34233ae29e92a137657c700c0edaa0c
Robin Hack <robin.hack@stud.uni-heidelberg.de>
2026-03-11 12:57:22 +0100

    feat: integrate dates module in index.js

--- public/js/index.js
+++ public/js/index.js
@@ -1,12 +1,15 @@
 import state from "./core/state.js"
 import { fetchTasks } from "./api/tasks.api.js";
 import renderCalendar from "./features/calendar/calendar.view.js";
+import dates from "./utils/dates.js"
 
 const init = async () => {
     try {
         // tasks in den state laden
         state.tasks = await fetchTasks();
-        renderCalendar.renderCalendar();     
+        renderCalendar.renderCalendar();
+        dates.setDate();
+ 
     } catch (err) {
         console.error("Init fehlgeschlagen:", err);
     }
