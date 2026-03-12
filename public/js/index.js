d229aae356c7a259ebccf99c69e8416b37e5a8cc
Robin Hack <robin.hack@stud.uni-heidelberg.de>
2026-03-12 12:03:46 +0100

    feat: integrate calendar logic in index.js

--- public/js/index.js
+++ public/js/index.js
@@ -2,6 +2,7 @@ import state from "./core/state.js"
 import { fetchTasks } from "./api/tasks.api.js";
 import renderCalendar from "./features/calendar/calendar.view.js";
 import dates from "./utils/dates.js"
+import calendarLogic from "./features/calendar/calendar.logic.js";
 
 const init = async () => {
     try {
@@ -9,6 +10,7 @@ const init = async () => {
         state.tasks = await fetchTasks();
         renderCalendar.renderCalendar();
         dates.setDate();
+        calendarLogic.renderThisMonth(state.selectedYear, state.selectedMonth);
  
     } catch (err) {
         console.error("Init fehlgeschlagen:", err);
