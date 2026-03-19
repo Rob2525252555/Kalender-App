9fa22543f03787cc2c48fbe2fa97e057548567cb
Robin Hack <robin.hack@stud.uni-heidelberg.de>
2026-03-19 12:54:16 +0100

    docs(modal): add JSDoc to modal.view and modal.events

--- public/js/features/modal/modal.events.js
+++ public/js/features/modal/modal.events.js
@@ -1,25 +1,48 @@
 import elements from "../../core/elements.js";
+/**
+ * @module modal.events
+ * Modul zum Anzeigen oder Verbergen des Overlays mit Modal.
+ * Aufgaben:
+ * - Overlay mit Modal anzeigen, nach Klick auf "+ Aufgabe hinzufügen"- Button
+ * - Modal verbergen bei Klick auf Close-Button, Klick auf das Overlay oder Drücken der Esc-Taste
+ */
 
 const modalEvents = {
 
+    /**
+     * Initialisiere alle Eventlistener für das Modal
+     */
     init(){  
         elements.addTaskButton.addEventListener('click', modalEvents.openModal);
         elements.overlayCloseButton.addEventListener('click', modalEvents.closeModal);
         elements.modalOverlay.addEventListener('click', modalEvents.handleOverlayClick);
         document.addEventListener('keydown', modalEvents.handleOverlayEsc);
     },
+    /**
+     * Modal anzeigen
+     */
     openModal(){
         elements.modalOverlay.classList.add('modal--active');
     },
-
+    /**
+     * Modal verbergen
+     */
     closeModal(){
         elements.modalOverlay.classList.remove('modal--active');
     },
+    /**
+     * Modal verbergen bei Klick auf das Overlay 
+     * @param {Event} e - Klick-Event
+     */
     handleOverlayClick(e){
         if(e.target === elements.modalOverlay){
             modalEvents.closeModal();
         }
     },
+    /**
+     * Modal verbergen bei drücken der Taste Esc
+     * @param {Event} e - Tastatur-Event 
+     */
     handleOverlayEsc(e){
         if(e.key === 'Escape' && elements.modalOverlay.classList.contains('modal--active')){
             modalEvents.closeModal();
--- public/js/features/modal/modal.view.js
+++ public/js/features/modal/modal.view.js
@@ -1,8 +1,23 @@
 import dom from "../../core/dom.js";
 import elements from "../../core/elements.js";
 
+/**
+ * @module modal.view
+ * Modul zum erstellen und Anzeigen eines Modals mit Overlay und Close-Button.
+ * 
+ * Aufgaben:
+ * - Rendert ein Overlay
+ * - Rendert auf dem Overlay ein Modal
+ * - Rendert ein Close-Button im Modal
+ * - Speichert die Referenzen von Overlay und Close-Button
+ */
+
  const createModal = {
 
+    /**
+     * Erstellt das Modal mit Overlay und Close-Button und
+     *  speichert die Referenzen in elements
+     */
     createModal(){
    
     const modalOverlay = dom.create({
@@ -22,7 +37,7 @@ import elements from "../../core/elements.js";
         parent: modal
     });
 
-    // Elemente in elements speichern
+    // Overlay und Close-Button in elements speichern
     elements.modalOverlay = modalOverlay;
     elements.overlayCloseButton = overlayCloseButton;
     }
