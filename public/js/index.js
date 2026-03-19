769a53cc34f105baefd1ec91029c9ff7f7c61b4e
Robin Hack <robin.hack@stud.uni-heidelberg.de>
2026-03-19 13:01:31 +0100

    feat(index): integrate modal creation and event handling

--- public/js/index.js
+++ public/js/index.js
@@ -4,23 +4,26 @@ import renderCalendar from "./features/calendar/calendar.view.js";
 import dates from "./utils/dates.js"
 import calendarLogic from "./features/calendar/calendar.logic.js";
 import calendarEvents from "./features/calendar/calendar.events.js";
-
 import tasksLogic from "./features/tasks/tasks.logic.js";
+import createModal from "./features/modal/modal.view.js";
+import modalEvents from "./features/modal/modal.events.js";
 
 const init = async () => {
     try {
         // tasks in den state laden
         state.tasks = await fetchTasks();
         renderCalendar.renderCalendar();
+
+        createModal.createModal();
+        modalEvents.init();
+
         dates.setDate();
+
         calendarLogic.renderThisMonth(state.selectedYear, state.selectedMonth);
         calendarEvents.init();
+
         tasksLogic.renderTasks();
-       
-        
-        
-       
-        
+
         
     } catch (err) {
         console.error("Init fehlgeschlagen:", err);
