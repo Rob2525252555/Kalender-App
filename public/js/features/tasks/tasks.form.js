c003e26e9c478e1395e75a334e27dd878b238985
Robin Hack <robin.hack@stud.uni-heidelberg.de>
2026-03-20 11:50:00 +0100

    feat(tasks.form): implement add task form with styles

---
+++ public/js/features/tasks/tasks.form.js
@@ -0,0 +1,147 @@
+import dom from "../../core/dom.js";
+import elements from "../../core/elements.js";
+
+const createTaskForm = {
+    createTaskForm() {
+        
+        // Task Formular erstellen
+        const taskForm = dom.create({
+            tagName: 'form',
+            classList: ['task-form'],
+            parent: elements.formModal
+        });
+
+        // --- Titel ---
+        const taskFormGroup1 = dom.create({
+            tagName: 'div',
+            classList: ['task-form-group'],
+            parent: taskForm
+        });
+
+        dom.create({
+            tagName: 'label',
+            classList: ['task-form-label'],
+            innerText: 'Titel',
+            htmlFor: 'taskTitle',
+            parent: taskFormGroup1
+        });
+
+        dom.create({
+            tagName: 'input',
+            classList: ['task-form-input'],
+            type: 'text',
+            id: 'taskTitle',
+            name: 'title',
+            required: true,
+            parent: taskFormGroup1
+        });
+
+        // --- Mitarbeiter ---
+        const taskFormGroup2 = dom.create({
+            tagName: 'div',
+            classList: ['task-form-group'],
+            parent: taskForm
+        });
+
+        dom.create({
+            tagName: 'label',
+            classList: ['task-form-label'],
+            innerText: 'Mitarbeiter',
+            htmlFor: 'taskEmployee',
+            parent: taskFormGroup2
+        });
+
+        dom.create({
+            tagName: 'input',
+            classList: ['task-form-input'],
+            type: 'text',
+            id: 'taskEmployee',
+            name: 'employee',
+            required: true,
+            parent: taskFormGroup2
+        });
+
+        // --- Startdatum ---
+        const taskFormGroup3 = dom.create({
+            tagName: 'div',
+            classList: ['task-form-group'],
+            parent: taskForm
+        });
+
+        dom.create({
+            tagName: 'label',
+            classList: ['task-form-label'],
+            innerText: 'Startdatum',
+            htmlFor: 'taskStart',
+            parent: taskFormGroup3
+        });
+
+        dom.create({
+            tagName: 'input',
+            classList: ['task-form-input'],
+            type: 'date',
+            id: 'taskStart',
+            name: 'startDate',
+            required: true,
+            parent: taskFormGroup3
+        });
+
+        // --- Enddatum ---
+        const taskFormGroup4 = dom.create({
+            tagName: 'div',
+            classList: ['task-form-group'],
+            parent: taskForm
+        });
+
+        dom.create({
+            tagName: 'label',
+            classList: ['task-form-label'],
+            innerText: 'Enddatum',
+            htmlFor: 'taskEnd',
+            parent: taskFormGroup4
+        });
+
+        dom.create({
+            tagName: 'input',
+            classList: ['task-form-input'],
+            type: 'date',
+            id: 'taskEnd',
+            name: 'endDate',
+            parent: taskFormGroup4
+        });
+
+        // --- Beschreibung ---
+        const taskFormGroup5 = dom.create({
+            tagName: 'div',
+            classList: ['task-form-group'],
+            parent: taskForm
+        });
+
+        dom.create({
+            tagName: 'label',
+            classList: ['task-form-label'],
+            innerText: 'Beschreibung',
+            htmlFor: 'taskDescription',
+            parent: taskFormGroup5
+        });
+
+        dom.create({
+            tagName: 'textarea',
+            classList: ['task-form-textarea'],
+            id: 'taskDescription',
+            name: 'description',
+            parent: taskFormGroup5
+        });
+
+        // --- Submit Button ---
+        const submitAddTaskButton = dom.create({
+            tagName: 'button',
+            classList: ['form-button'],
+            type: 'submit',
+            innerText: 'Aufgabe erstellen',
+            parent: taskForm
+        });       
+    }
+};
+
+export default createTaskForm;
