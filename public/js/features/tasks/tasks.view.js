630fd0048883b4202e09e734b99bda4841eb0897
RobinH111 <robin.hack@stud.uni-heidelberg.de>
2026-04-15 13:23:32 +0200

    refactor(tasks.view): improve comments and remove unused code

--- public/js/features/tasks/tasks.view.js
+++ public/js/features/tasks/tasks.view.js
@@ -5,112 +5,91 @@ import elements from "../../core/elements.js"
  * @module tasks.view
  * Modul zum Rendern von Tasks innerhalb von Kalenderzellen.
  * 
- * Aufgaben:
- * - Rendern von Titel, Mitarbeiter und Buttons (Details, Bearbeiten, Löschen)
- * - Wird die Task am Enddatum gerendert, wird zusätzlich die Klasse 'endDateTask' hinzugefügt
- * - Referenz des taskContainers, ID, Startdatum und Enddatum der Task wird in elements.tasksElements gespeichert
+ * Jede task besteht aus:
+ * - Titel
+ * - Mitarbeiter
+ * - Buttons (Details, Bearbeiten, Löschen)
+ * 
+ * Beim Rendern am Enddatum wird die Klasse 'endDateTask' hinzugefügt,
+ * für einen roten Hintergrund.
+ * Nach dem Rendern wird die Referenz des Task-Containers und die zugehörige ID in elements.tasksElements gespeichert.
+ * 
+ * @param {Object} task - Task-Objekt aus dem State    
+ * @param {HTMLElement} parentCell - Kalenderzelle, in die die Task gerendert werden soll
+ * @param {boolean} [isEndDate=false] - Gibt an, ob eine Task am Enddatum oder Startdatum gerendert wird
  */
 
+export function createTaskElement(task, parentCell, isEndDate = false) {
 
+    const taskContainer = dom.create({
+        tagName: 'div',
+        classList: isEndDate ? ['task', 'endDateTask'] : ['task'],
+        parent: parentCell
+    });
 
-    /**
-     * Rendert eine Task innerhalb einer ausgewählten Kalenderzelle 
-     * und speichert die Referenz.
-     * Wird die Task am Enddatum gerendert, wird zusätzlich die Klasse 'endDateTask' hinzugefügt.
-     * 
-     * Aufbau:
-     * taskContainer
-     *      TaskHeader -> TaskTitle, TaskButtonsContainer -> Details-Button, Edit-Button, Delete-Button
-     *      TaskEmployee
-     * 
-     * @param {Object} task - Task-Objekt aus dem State    
-     * @param {HTMLElement} parentCell - Kalenderzelle, in die die Task gerendert werden soll
-     * @param {boolean} [isEndDate=false] - Gibt an, ob eine Task am Enddatum oder Startdatum gerendert wird
-     */
+    const taskHeader = dom.create({
+        tagName: 'div',
+        classList: ['taskHeader'],
+        parent: taskContainer
+    });
 
-    export function createTaskElement(task, parentCell, isEndDate = false){
+    dom.create({
+        tagName: 'div',
+        classList: ['taskEmployee'],
+        innerText: task.employee,
+        parent: taskContainer
+    });
 
-            const taskContainer = dom.create({
-            tagName: 'div',
-            classList: isEndDate? ['task', 'endDateTask'] : ['task'],
-            dataset:{
-                id: task.id,
-                startDate: task.startDate,
-                endDate: task.endDate
-            },
-            parent: parentCell,
-        })
-        
-        const taskHeader = dom.create({
-            tagName: 'div',
-            classList: ['taskHeader'],
-            parent: taskContainer
-        })
+    dom.create({
+        tagName: 'span',
+        classList: ['taskTitle'],
+        innerText: task.title,
+        title: task.title,
+        parent: taskHeader
+    });
 
-        const taskEmployee = dom.create({
-            tagName: 'div',
-            classList: ['taskEmployee'],
-            innerText: task.employee,
-            parent: taskContainer
-        })
+    const taskButtonsContainer = dom.create({
+        tagName: 'div',
+        classList: ['taskButtons'],
+        parent: taskHeader
+    });
 
-        const taskTitle = dom.create({
-            tagName: 'span',
-            classList: ['taskTitle'],
-            innerText: task.title,
-            title: task.title,
-            parent: taskHeader
-        })
-
-        const taskButtonsContainer = dom.create({
-            tagName: 'div',
-            classList: ['taskButtons'],
-            parent: taskHeader
-        })
-
-        const detailsButton = dom.create({
-            tagName: 'button',
-            title: 'Details',
-            innerText: '🔍',
-            dataset: {
-                id: task.id,
-                action: 'detailsButton'
-            },
-            parent:taskButtonsContainer 
-        })
-
-        const editButton = dom.create({
-            tagName: 'button',
-            title: 'Bearbeiten',
-            innerText: '✏️',
-            dataset: {
-                id: task.id,
-                action: 'editButton'
-            },
-            parent:taskButtonsContainer 
-        })
-
-        const deleteButton = dom.create({
-            tagName: 'button',
-            title: 'Löschen',
-            innerText: '✕',
-            dataset: {
-                id: task.id,
-                action: 'deleteButton'
-            },
-            parent:taskButtonsContainer 
-        })
-
-        // taskContainer mit zugehöriger ID und Buttons in elements speichern
-        elements.tasksElements.push({
+    dom.create({
+        tagName: 'button',
+        title: 'Details',
+        innerText: '🔍',
+        dataset: {
             id: task.id,
-            container: taskContainer,
-            startDate: task.startDate,
-            endDate: task.endDate,
-            buttons: {
-                details: detailsButton,
-                edit: editButton,
-                delete: deleteButton
-            }
-        })    
-    };
+            action: 'detailsButton'
+        },
+        parent: taskButtonsContainer
+    });
+
+    dom.create({
+        tagName: 'button',
+        title: 'Bearbeiten',
+        innerText: '✏️',
+        dataset: {
+            id: task.id,
+            action: 'editButton'
+        },
+        parent: taskButtonsContainer
+    });
+
+    dom.create({
+        tagName: 'button',
+        title: 'Löschen',
+        innerText: '✕',
+        dataset: {
+            id: task.id,
+            action: 'deleteButton'
+        },
+        parent: taskButtonsContainer
+    });
+
+    // taskContainer mit zugehöriger ID und Buttons in elements speichern
+    elements.tasksElements.push({
+        id: task.id,
+        container: taskContainer,
+    });
+};
